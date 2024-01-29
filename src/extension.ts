90296681db3c0a8b8151ce1435634804cf2428f0
Yeonghun Harry Ki <yeonghun.ki@gmail.com>
2024-01-28 22:47:34 -0800

    fixed path parsing logic

--- src/extension.ts
+++ src/extension.ts
@@ -5,31 +5,45 @@ import fs from 'fs';
 import path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "yaml-navigator" is now active!');
+	// console.log('Congratulations, your extension "yaml-navigator" is now active!');
 	let openFileOnPath = vscode.commands.registerCommand('yaml-navigator.openFileOnPath', function () {
 		// The code you place here will be executed every time your command is executed
 		let editor = vscode.window.activeTextEditor;
 
 		// TODO: allow user to configure the regex
-		let regexTon = new RegExp(/(?:\.\.|\.)*\/\S+\.\w*/);
+		let regexTon = new RegExp(/\S+\.\w*/);
 		if (!editor) return;
 		//Get the selection starting from the cursor position and searching for a regular expression
+		// console.log("position: ", editor.selection.active)
 		let range = editor.document.getWordRangeAtPosition(editor.selection.active, regexTon);
-		if (range) {
-			var currentlyOpenTabfilePath = editor.document.fileName;
-			//Get the pure match against the regualr expression
-			let text = editor.document.getText(range);
+		// let range = editor.document.getWordRangeAtPosition(editor.selection.active);
+		if (!range) {
+			vscode.window.showErrorMessage(`no valid file path found with ${regexTon}`)
+			return;
+		}
 
-			var resolvedPath;
+		var currentlyOpenTabfilePath = editor.document.fileName;
+		//Get the pure match against the regualr expression
+		let text = editor.document.getText(range);
 
-			// possible cases
-			// 1. /cicd/common/setup.yml -> this should be navigated to $project_root/cicd/common/setup.yml
-			// 2. ../common/setup.yml -> this should be navigated to parentdirectory/common/setup.yml
-			// 3. ./variables.yaml -> this should be navigated to currentdirectory/variables.yaml
+		var resolvedPath;
+
+		// possible cases
+		// Template paths can be an absolute path within the repository or relative to the file that does the including.
+		//To use an absolute path, the template path must start with a /. All other paths are considered relative.
+
+		// if first letter starts with `/`, regard it as absolute path from project
+		if (text[0] === '/') {
+			resolvedPath = text;
+			// get the project root
+			let projectRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+			if (projectRoot) {
+				resolvedPath = path.join(projectRoot, text);
+			}
+		} else {
+			// relative path
 			let segments = text.split("/");
 			if (segments[0] == "..") {
-				// relative path
-				// resolvedPath = path.join(path.dirname(currentlyOpenTabfilePath), ...segments);
 				// loop segments and find how many upper directory we need to go
 				let upperDirCount = 0;
 				for (let i = 0; i < segments.length; i++) {
@@ -45,32 +59,23 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 				segments.splice(0, upperDirCount);
 				resolvedPath = path.join(parentDir, ...segments);
-			} else if (segments[0] == "." ){
-				// relative path
-				console.log("relative from current", segments)
-				resolvedPath = path.join(path.dirname(currentlyOpenTabfilePath), ...segments);
 			} else {
-				resolvedPath = text;
-				// get the project root
-				let projectRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-				if (projectRoot) {
-					resolvedPath = path.join(projectRoot, text);
-				}
+				// relative path
+				// console.log("relative from current", segments)
+				resolvedPath = path.join(path.dirname(currentlyOpenTabfilePath), ...segments);
 			}
-
-			if (!resolvedPath || !fs.existsSync(resolvedPath)) {
-				vscode.window.showErrorMessage(`File does not exist in ${resolvedPath}, matched file path: ${text}`)
-				return;
-			}
-
-			let url = vscode.Uri.file(resolvedPath);
-			vscode.commands.executeCommand('vscode.open', url);
 		}
 
-		
+		if (!resolvedPath || !fs.existsSync(resolvedPath)) {
+			vscode.window.showErrorMessage(`File does not exist in ${resolvedPath}, matched file path: ${text}`)
+			return;
+		}
+
+		let url = vscode.Uri.file(resolvedPath);
+		vscode.commands.executeCommand('vscode.open', url);
 	});
 
 	context.subscriptions.push(openFileOnPath);
 }
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
